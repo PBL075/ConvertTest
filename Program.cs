5f6a64ec17c3d7c7a7baa22044b4854b20078582
PBL075 <pavelgorea75@gmail.com>
2026-04-26 10:34:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace test_convert
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Introdu numarul de cai putere (HP): ");
+            int hpStock = Convert.ToInt32 (Console.ReadLine());
+            int hpCuTuning = hpStock + 50 ; 
+
+            Console.WriteLine("Daca mai punem o turbina , vei avea: " + hpCuTuning + "HP");
+
+        }
+    }
+}
